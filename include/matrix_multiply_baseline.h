a8abae94afd40d65e4f6c714b66626efd47bedf2
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-11-30 23:11:16 +0100

    Keep only multiplyMatrices function

--- include/matrix_multiply_baseline.h
+++ include/matrix_multiply_baseline.h
@@ -1,11 +1,9 @@
 #ifndef MATRIX_MULTIPLY_BASELINE_H
 #define MATRIX_MULTIPLY_BASELINE_H
 
+#include "matrix.h"
 #include <vector>
 
-typedef std::vector<std::vector<int>> Matrix;
+Matrix multiplyMatrices(const Matrix &a, const Matrix &b);
 
-Matrix multiplyMatrices(const Matrix& a, const Matrix& b);
-void printMatrix(const Matrix& matrix);
-
-#endif // MATRIX_MULTIPLY_BASELINE_H
+#endif
